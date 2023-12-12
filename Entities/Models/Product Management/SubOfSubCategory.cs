af7bb5ffa52fec1149b098dd9bd80ba73881451b
mustafa.ware@gmail.com <mustafa.ware@gmail.com>
2023-12-12 23:20:27 +0400

    update product sub cateogries tables

--- Entities/Models/Product Management/SubOfSubCategory.cs
+++ Entities/Models/Product Management/SubOfSubCategory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,9 @@ namespace Entities.Models.Product_Management
         public int Id { get; set; }
         public string? CategoryName { get; set; }
         public string? CategoryDescritpion { get; set; }
+        public int SubCategoryNo { get; set; }
+        [ForeignKey("SubCategoryNo")]
+        public virtual SubCategory? SubCategory { get; set; }
         public DateTime CategoryCreatedDate { get; set; }
         public DateTime CategoryModifiedDate { get; set; }
 
